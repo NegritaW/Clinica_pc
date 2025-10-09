65950919414ebf8d551e9dea1fc22692c1dd5b39
NegritaW <ivypra8840@gmail.com>
2025-10-09 15:11:56 -0300

    ModeloLoginyEntrega

--- entrega/models.py
+++ entrega/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Entrega(models.Model):
+    nombre_cliente = models.CharField(max_length=100)
+    tipo_equipo = models.CharField(max_length=100)
+    problema_reportado = models.TextField()
+    tecnico_asignado = models.CharField(max_length=100)
+    estado = models.CharField(max_length=20, default="entregado")
+    observaciones = models.TextField(blank=True)
+    fecha_entrega = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Entrega de {self.nombre_cliente} ({self.estado})"
