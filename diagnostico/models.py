5a9a62f0539a7f08d13dd1ce8015ab2624c0b8d7
Bentorrs <benjatorres31@gmail.com>
2025-10-09 15:18:16 -0300

    Modelos de Recepcion y Diagnostico

--- diagnostico/models.py
+++ diagnostico/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Diagnostico(models.Model):
+  ESTADO_CHOICES = [
+        ('asignado', 'Asignado'),
+        ('en_progreso', 'En progreso'),
+        ('finalizado', 'Finalizado'),
+  ]
+
+  equipo = models.CharField(max_length=100)
+  diagnostico_text = models.TextField(blank=True)
+  solucion_text = models.TextField(blank=True)
+  estado = models.CharField(max_length=20, choices=ESTADO_CHOICES, default='asignado')
+  creado = models.DateTimeField(auto_now_add=True)
+  actualizado = models.DateTimeField(auto_now=True)
