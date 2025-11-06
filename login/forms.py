9010589c2a4d94b449bcb956b1be3fd32a9546a1
NegritaW <ivypra8840@gmail.com>
2025-11-06 15:33:36 -0300

    Actualizacion de login para integracion casi completa

--- login/forms.py
+++ login/forms.py
@@ -1,4 +1,3 @@
-'''
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import Usuario
@@ -55,4 +54,3 @@ class AsignarRolForm(forms.ModelForm):
             'rol': 'Rol del usuario',
             'is_active': 'Activo',
         }
-'''
--- login/urls.py
+++ login/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('protected/', views.protected_view, name='protected'),
+    path('loginregistro/', views.register_view, name='loginregistro'),
 ]
